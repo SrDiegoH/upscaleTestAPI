d4fb1453899bc7feea880db802acb10b885518b3
SrDiegoH <diegohenrique4222@gmail.com>
2023-07-22 18:28:59 -0300

    test: test deploy with new endpoint

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, send_file
 
-from .interpolation_upscale import apply_upscale, InterpolationType
+#from .interpolation_upscale import apply_upscale, InterpolationType
 
 app = Flask(__name__)
 
@@ -10,7 +10,6 @@ def root():
 
 @app.route('/', methods=['POST'])
 def upscale():
-    '''
     image = request.files('image')
 
     upscale_type = request.values.get('upscale_type').encode("UTF-8")
@@ -21,12 +20,13 @@ def upscale():
     blur_intensity = int(request.values.get('blur_intensity').encode("UTF-8"))
     blur_type = request.values.get('blur_type').encode("UTF-8")
 
+    '''
     if upscale_type in dir(InterpolationType):
         upscaled_image = apply_upscale(upscale_type, image, scale_factor, denoise_intensity, blur_intensity, blur_type)
 
     return send_file(upscaled_image)
     '''
-    pass
+    return 'UPSCALE'
 
 if __name__ == '__main__':
     app.run()
