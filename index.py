4b70510b6437e8264c2774443dcabb7d8ccfdf98
SrDiegoH <diegohenrique4222@gmail.com>
2023-07-29 13:09:40 -0300

    test: test deploy with new endpoint

--- index.py
+++ index.py
@@ -78,10 +78,10 @@ def upscale():
     blur_type = request.values.get('blur_type')
 
     #if upscale_type in dir(InterpolationType):
-    upscaled_image = apply_upscale(upscale_type, image, scale_factor, denoise_intensity, blur_intensity, blur_type)
+    #upscaled_image = apply_upscale(upscale_type, image_bytes, scale_factor, denoise_intensity, blur_intensity, blur_type)
 
     #return send_file(upscaled_image)
-    return f'Upscale type: {upscale_type}, Scale factor: {scale_factor}, Denoise intensity: {denoise_intensity}, Blur intensity: {blur_intensity}, Blur type: {blur_type}, Image: {image}'
+    return f'Upscale type: {upscale_type}, Scale factor: {scale_factor}, Denoise intensity: {denoise_intensity}, Blur intensity: {blur_intensity}, Blur type: {blur_type}, Image: {image}, Image Bytes: {image_bytes}, Upscaled Image: {upscaled_image}'
 
 if __name__ == '__main__':
     app.run()
