b306f3d8053541e90960ed9ed840d9a0eb6fb7ca
SrDiegoH <diegohenrique4222@gmail.com>
2024-09-29 23:20:20 -0300

    test: try to use process to avoid timeout

--- index.py
+++ index.py
@@ -285,12 +285,6 @@ def apply_super_resolution(super_resolution_type, image_buffer, denoise_intensit
     return blurred_image
 
 def process_and_save_on_cache(random_uuid, upscale_type, image_buffer, denoise_intensity, blur_intensity, blur_type, scale_factor):
-    import time
-    count = 0
-    while True:
-        count += 1
-        print(f'Separated process loop n# {count}')
-        time.sleep(0.001)
     try:
         if upscale_type in dir(InterpolationType):
             upscaled_image = apply_upscale(upscale_type, image_buffer, denoise_intensity, blur_intensity, blur_type, scale_factor)
