99a51f94148e2f297086abcba8b8a8410d1f333a
SrDiegoH <diegohenrique4222@gmail.com>
2024-09-29 22:37:12 -0300

    test

--- index.py
+++ index.py
@@ -278,7 +278,7 @@ def apply_super_resolution(super_resolution_type, image_buffer, denoise_intensit
 
 def process_and_save_on_cache(random_uuid, upscale_type, image_buffer, denoise_intensity, blur_intensity, blur_type, scale_factor):
     try:
-        print(f'Start')
+        print(f'Start process_and_save_on_cache')
         if upscale_type in dir(InterpolationType):
             upscaled_image = apply_upscale(upscale_type, image_buffer, denoise_intensity, blur_intensity, blur_type, scale_factor)
         elif upscale_type and f'_{upscale_type.strip()}' in dir(SuperResolutionType):
@@ -332,6 +332,7 @@ def upscale():
         args=(random_uuid, upscale_type, image_buffer, denoise_intensity, blur_intensity, blur_type, scale_factor)
     ).start()
     """
+    print(f'starting trhead')
     multiprocessing.Process(
         target=process_and_save_on_cache,
         args=(random_uuid, upscale_type, image_buffer, denoise_intensity, blur_intensity, blur_type, scale_factor)
